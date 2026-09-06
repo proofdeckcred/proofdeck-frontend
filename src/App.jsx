ec328ac76aa2839b9d5d91defd7cc93641c1497d
Omobolaji Durojaiye <99079690+OmobolajiDurojaiye@users.noreply.github.com>
2026-09-06 21:43:33 +0100

    Temporarily disable SupportWidget

--- src/App.jsx
+++ src/App.jsx
@@ -175,7 +175,7 @@ function App() {
 
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-        <SupportWidget />
+        {/* <SupportWidget /> */}
       </AdminAuthProvider>
     </UserProvider>
   );
